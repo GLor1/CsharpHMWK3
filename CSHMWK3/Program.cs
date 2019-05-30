dae34f2037cbd696031d58101155a5f822ddebf4
GLor1 <50379998+GLor1@users.noreply.github.com>
2019-05-30 13:07:48 -0500

    Add files via upload

---
+++ CSHMWK3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSHMWK3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Supercar Make = new Supercar("Lotus Elise", "NA", "Black");
+                      
+        }
+    }
+    public class Supercar
+    {
+        public static string _makeModel { set; get; }
+        public static string _engine { set; get; }
+        public static string _color { set; get; }//can we randomize it?
+
+        public Supercar(string makeModel, string engine, string color)
+        {
+            _makeModel = makeModel;
+            _engine = engine;
+            _color = color;
+
+            Console.WriteLine("The car you've ordered has arrived {0}.", makeModel);
+            Console.WriteLine("The engine your car has is {0}.", engine);
+            Console.WriteLine("The color of you car is {0}.", color);
+
+        }
+
+    }
+}
